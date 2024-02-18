a395a3c58f92353af45e57680a935b30543ed7ae
vince <vincecole6@hotmail.co.uk>
2024-02-18 14:04:02 +0000

    Add in test to make sure the endpoint defaults. Don't really need this but in the event someone wants to test and I can't finish the front end, we can at least rely on the backup.

--- api/tests/Unit/AWSIntegrationTest.php
+++ api/tests/Unit/AWSIntegrationTest.php
@@ -21,7 +21,7 @@ class AWSIntegrationTest extends TestCase
 
     /**
      * @test
-     * Fake an API call to the Textract API and handle the response
+     * It can convert a PDF file (or any file really as our validation will occur in the form request) to B64
      */
     public function it_can_find_a_locally_stored_pdf_and_convert_it_to_pdf()
     {
@@ -33,6 +33,16 @@ class AWSIntegrationTest extends TestCase
         assertTrue(base64_encode(base64_decode($pdf, true)) === $pdf);
     }
 
+    /**
+     * @test 
+     * It will search the local directory for sample.pdf if no file is provided and use that as a default
+     */
+    public function it_can_find_the_default_file_if_no_file_is_provided(){
+        $PDFService = new PDFService();
+        $pdf = $PDFService->convertPDFToB64();
 
+        //Ascertain whether it's base64 encoded
+        assertTrue(base64_encode(base64_decode($pdf, true)) === $pdf);
+    }
 
 }
