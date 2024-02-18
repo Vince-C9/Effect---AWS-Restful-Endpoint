37c321ff27d0f7807068e2b34308a136e3b70193
vince <vincecole6@hotmail.co.uk>
2024-02-18 16:31:16 +0000

    Set up relationships

--- api/app/Models/PDFChunks.php
+++ api/app/Models/PDFChunks.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PDFChunks extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'p_d_f_contents_id',
+        'block_data',
+        'text',
+        'block_type',
+        'confidence',
+    ];
+
+
+    /**
+     * Associate to parent content
+     *
+     * @return BelongsTo
+     */
+    public function PDFContent(): BelongsTo
+    {
+        return $this->belongsTo(PDFcontent::class);
+    }
+
 }
--- api/app/Models/PDFContent.php
+++ api/app/Models/PDFContent.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class PDFContent extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'pages',
+    ];
+
+
+    /**
+     * Get all associated blocks (chunks)
+     *
+     * @return HasMany
+     */
+    public function PDFChunks(): HasMany
+    {
+        return $this->hasMany(PDFChunks::class);
+    }
 }
