e452f95b5a618c6dc2a1e8a3b10188cd73888b16
vince <vincecole6@hotmail.co.uk>
2024-02-18 06:26:07 +0000

    Setup basic scaffolding shell

---
+++ api/app/Http/Controllers/PDFContentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PDFUploadRequest;
+use Illuminate\Http\Request;
+
+class PDFContentController extends Controller
+{
+
+    public function store(PDFUploadRequest $request){
+
+    }
+}
