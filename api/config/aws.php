b9164edd678c819a52e1ce9f5ff556be5abf458e
vince <vincecole6@hotmail.co.uk>
2024-02-18 05:35:51 +0000

    strip unwanted from aws config

--- api/config/aws.php
+++ api/config/aws.php
@@ -2,7 +2,6 @@
 
 return [
     'aws_name' => env('AWS_TEXTRACT_ACCESS_KEY',''),
-    'aws_secret' => env('AWS_TEXTRACT_SECRET','')
+    'aws_secret' => env('AWS_TEXTRACT_SECRET',''),
     'aws_region'=> env('AWS_REGION'),
-    'aws_'
 ];
